4c7256d6f7e82ba7da863abcc88d63a43bab7cf0
chuchurex <chuchurex@gmail.com>
2026-03-04 10:04:21 -0300

    refactor: migrate build system to @eluno/core v2
    Replace monolithic build-v2.cjs (1675 lines) with modular @eluno/core v2.
    Build now runs via `eluno-build` CLI from core, SCSS compiled from core.
    
    - Add eluno.config.js with all features enabled
    - Rewrite ui.json files to flat v2 format (was nested v1)
    - Add @eluno/core#v2 as dependency
    - Remove scripts/build-v2.cjs, src/scss/, src/js/, src/fonts/
      (all now live in core, -4768 lines)

---
+++ eluno.config.js
@@ -0,0 +1,25 @@
+export default {
+  siteUrl: 'https://eluno.org',
+  languages: ['en', 'es', 'pt'],
+  baseLang: 'en',
+  baseLangPrefix: true,
+  bookTitles: { en: 'The One', es: 'El Uno', pt: 'O Um' },
+  chapterUrlPattern: 'slug',
+  enabledChapters: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+
+  lawOfOneUrls: {
+    en: 'https://www.lawofone.info/s/',
+    es: 'https://es.lawofone.info/s/',
+    pt: 'https://www.lawofone.info/s/'
+  },
+
+  features: {
+    glossary: true,
+    glossaryCategories: true,
+    provenance: true,
+    search: true,
+    mediaToolbar: true,
+    termMarkup: true,
+    refMarkup: true
+  }
+};
